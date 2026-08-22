aeeaedb3adc6cab03a06c3a513703aa9c770236b
FauzanAzima26 <fauzannazimaaa123@gmail.com>
2026-08-22 14:24:25 +0700

    menyiapkan backend untuk deployment

--- backend/app.py
+++ backend/app.py
@@ -273,8 +273,10 @@ def classify():
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=port,
+        debug=False
     )
